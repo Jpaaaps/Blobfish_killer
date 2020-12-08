81f3fbcdca57169853eb9f26eda3fe9da2426659
jpaaaps <jpgerard87@gmail.com>
2020-12-08 17:03:01 +0100

    adding footer and modification in the form and and back folder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,45 +9,45 @@ function Footer() {
           GO FISH YOURSELF
         </p>
       </section>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='social-icons'>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='social-icons'>
 					<a
-              class='social-icon-link facebook'
+              className='social-icon-link facebook'
               href='https://www.facebook.com/blobfishkiller'
               target='_blank'
               rel="noreferrer"
               aria-label='Facebook'
             >
-              <i class='fab fa-facebook-f' />
+              <i className='fab fa-facebook-f' />
             </a>
             <a
-              class='social-icon-link instagram'
+              className='social-icon-link instagram'
               href='https://www.instagram.com/jpaaaps/?hl=fr'
               target='_blank'
               rel="noreferrer"
               aria-label='Instagram'
             >
-              <i class='fab fa-instagram' />
+              <i className='fab fa-instagram' />
             </a>
             <a
-              class='social-icon-link youtube'
+              className='social-icon-link youtube'
               href='https://www.youtube.com/channel/UC6aa1_W6O_Vm89WE5jLLKtQ'
               target='_blank'
               rel="noreferrer"
               aria-label='Youtube'
             >
-              <i class='fab fa-youtube' />
+              <i className='fab fa-youtube' />
             </a>
           </div>
-					<div class='footer-logo'>
+					<div className='footer-logo'>
 						<img className='logoblob3' src='/photos/blobagram.png' alt='logo blob pentagram' />
 					</div>
-					<small class='website-rights'>JP GERARD © 2020</small>
+					<small className='website-rights'>JP GERARD © 2020</small>
         </div>
       </section>
     </div>
   );
 }
 
-export default Footer;
+export default Footer
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,90 @@
-import React from "react"
+import React, { useState } from "react"
 import { useForm } from "react-hook-form"
 import ErrorMessage from './ErrorMessage'
+import axios from 'axios'
 
 import './Form.css'
 
 const Form = () => {
+  const [data, setData] = useState({ firstName: '', lastName: '', email: '', message: '', sent: false })
 
   const {
     register,
-    handleSubmit,
     errors,
-    setError,
-    clearError,
-    formState: { isSubmitting }
-	} = useForm()
+  } = useForm()
+  
+  const handleChange = (e) => {
+    const {name, value} = e.target
+        setData({
+            ...data,
+            [name]: value
+    })
+  }
 	
-  const onSubmit = data => {
-    alert(JSON.stringify(data));
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios({
+      method: "POST", 
+      url:"/contact", 
+      data: data
+    }).then((res)=>{
+      if (res.data.status === 'success'){
+          alert("Message Sent."); 
+          resetForm()
+      }else if(res.data.status === 'fail'){
+          alert("Message failed to send.")
+      }
+    })
+}
+const resetForm = () => {
+    this.setState({firstName: '', lastName: '', email: '',subject:'', message: ''})
+  }
 
   return (
     <div className="form_container">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit}>
         
         <label>First Name</label>
-        <input name="firstName" ref={register({ required: true })} />
+        <input 
+          name="firstName" 
+          ref={register({ required: true })}
+          onChange={handleChange}
+          value={data.firstName}
+        />
         <ErrorMessage error={errors.firstName} />
 
         <label>Last Name</label>
-        <input name="lastName" ref={register({ required: true, minLength: 2 })} />
+        <input 
+          name="lastName" 
+          ref={register({ required: true, minLength: 2 })}
+          onChange={handleChange}
+          value={data.lastName}
+        />
         <ErrorMessage error={errors.lastName} />
 
         <label>Email</label>
         <input
           name="email"
           ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+          onChange={handleChange}
+          value={data.email}
         />
         <ErrorMessage error={errors.email} />
 
         <label>A message for us ?</label>
-        <textarea name="aboutyou" ref={register({ required: true })} />
+        <textarea 
+          name="message"  
+          ref={register({ required: true })}
+          value={data.message}
+          onChange={handleChange}
+          rows='3'
+        />
         <ErrorMessage error={errors.firstName} />
 
-        <button type="submit" disabled={isSubmitting}>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
 }
+
 export default Form
